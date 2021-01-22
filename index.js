3ee926bc50991ba1ae3887737b71890821a91d89
urusha12 <urushasakha@gmail.com>
2021-01-23 00:48:59 +0545

    Removed string data and added ordered list and added validation

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 let task = ["Task 1", "Task 2"];
 window.onload = function () {
+    let orderedList = "<ol id='list'>";
     task.forEach((item) => {
-        console.log(item);
-        document.getElementById("task").innerHTML += item;
+        orderedList += '<li>' + item + '</li>';
     }
     )
+    orderedList += '</ol>';
+    document.getElementById("task").innerHTML += orderedList;
 }
 
-const addData=()=>{
-    const inputData=document.getElementById("input_data").value;
-    task.push(inputData);
-    document.getElementById("task").innerHTML=task;
+const addData = () => {
+    const inputData = document.getElementById("input_data").value;
+    if (inputData === "") {
+        alert("Cannot add empty task");
+    }
+    else {
+        task.push(inputData);
+        var ol = document.getElementById("list");
+        let li = document.createElement("li");
+        li.appendChild(document.createTextNode(inputData));
+        ol.appendChild(li);
+        document.getElementById("input_data").value="";
+    }
 }
+
+
