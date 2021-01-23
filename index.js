c4620f10cd2343ce1ba0b0de77439aa38ed0aaad
urusha12 <urushasakha@gmail.com>
2021-01-24 01:17:37 +0545

    Modified task array from string to object, added uuid for uuid for unique id, added onclick event for checkbox input to change class name of parent element

--- index.js
+++ index.js
@@ -1,27 +1,64 @@
-let task = ["Task 1", "Task 2"];
+let task = [
+    {id: 1, title: 'Task 1', isCompleted: true}, 
+    {id: 2, title: 'Task 2', isCompleted: false}, 
+    {id: 3, title: 'Task 3', isCompleted: false}
+];
+
+const isChecked=(event,element)=>{
+    console.log(event);
+    console.log(element.checked);
+    if(element.checked)
+    {
+        
+        event.currentTarget.parentElement.classList = 'checked';
+        //console.log(element,"inside if");
+    }
+    else
+    {
+        event.currentTarget.parentElement.classList = 'unchecked';
+        //console.log(element,"inside else");
+    }
+}
+
 window.onload = function () {
-    let orderedList = "<ol id='list'>";
+    let unOrderedList = "<ul id='list'>";
     task.forEach((item) => {
-        orderedList += '<li>' + item + '</li>';
+        // unOrderedList += '<li>'+'<input type="checkbox" value="unchecked">' + item.title + '</li>';
+        unOrderedList += `<li class=${item.isCompleted===true?"checked":"unchecked"}> <input type="checkbox" ${item.isCompleted===true?"checked":""} onclick="isChecked(event,this)" > ${item.title} </li> `;
     }
     )
-    orderedList += '</ol>';
-    document.getElementById("task").innerHTML += orderedList;
+    unOrderedList += '</ul>';
+    document.getElementById("task").innerHTML += unOrderedList;
 }
 
 const addData = () => {
     const inputData = document.getElementById("input_data").value;
+    //console.log(inputData);
     if (inputData === "") {
         alert("Cannot add empty task");
     }
     else {
-        task.push(inputData);
-        var ol = document.getElementById("list");
+        
+        let uuid=uuidv4();
+        const newTask= {id:uuid,title:inputData,isCompleted:false};
+        task.push(newTask);
+        console.log(task);
+        let ul = document.getElementById("list");
         let li = document.createElement("li");
+        let checkBox= document.createElement('input');
+        checkBox.type="checkbox";
+        checkBox.value=0;
+        if(newTask.isCompleted)
+        {
+            checkBox.setAttribute("checked");
+        }
+        checkBox.onclick=function(){isChecked(event,this)};
+        li.appendChild(checkBox);
         li.appendChild(document.createTextNode(inputData));
-        ol.appendChild(li);
+        ul.appendChild(li);
         document.getElementById("input_data").value="";
     }
 }
+
 
 
